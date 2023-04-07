f5de0425cb72347b9b55a70fa61a0b082e83552c
Maxim Donskoy <maxwebdevelop@yandex.ru>
2023-04-07 11:58:50 +0300

    up

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json("res");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => console.log("server start"));
